8cbe308e9d41ce3a4c6f1326c4cbd22313f83205
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 15:43:26 +0700

    Tugas 15 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas15.js
@@ -0,0 +1,18 @@
+function tugas15() {
+  //! c? = 8/2 + 6/2
+  var a2 = Math.pow(8, 2);
+  var b2 = Math.pow(6, 2);
+  var C = a2 + b2;
+
+  console.log("Nilai AB :", Math.sqrt(a2));
+  console.log("Nilai CB :", Math.sqrt(b2));
+  console.log(
+    "Panjang sisi AC pada segitiga siku-siku tersebut adalah :",
+    Math.sqrt(C),
+    "cm"
+  );
+
+  /////////
+}
+
+tugas15();
